86c89c5923c4dc54f9b5d8c2318189d69960771b
KungBao <96529195+KungBao82045@users.noreply.github.com>
2023-01-14 13:08:12 +0100

    Update logged.php

--- Websites/Projects/Login:Signup/logged.php
+++ Websites/Projects/Login:Signup/logged.php
@@ -1,34 +1,6 @@
 <?php
 session_start();
 
-// if(isset($_POST['submit'])) {
-//     $Username = $_POST['Username'];
-//     $Password = $_POST['Password'];
-
-//     // Connect to the database
-
-//     $conn = new mysqli("127.0.0.1", "root", "root", "testing");
-
-//     // Retrieve the user from the database
-//     $query = "SELECT * FROM User_Test WHERE Username='$Username' AND Password='$Password'";
-//     $result = mysqli_query($conn, $query);
-
-//     if (!$result) {
-//         die("Error: " . mysqli_error($conn));
-//     } else if(mysqli_num_rows($result) == 1) {
-//         $_SESSION['Username'] = $Username;
-//         echo "<h1>Logged in as " . $Username . "</h1>";
-//         echo "<a href='login.php'>Log Out</a>";
-//         exit();
-//     }else{
-//         $_SESSION['error'] = "Invalid login credentials";
-//         header("location: failedLog.php");
-
-
-//     }
-// }
-
-
 
 if (isset($_POST['submit'])) {
     $Username = $_POST['Username'];

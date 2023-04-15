a04faddf64c7b8402a191167ffef40b29ec8e7ab
KungBao <96529195+KungBao82045@users.noreply.github.com>
2023-04-15 12:07:23 +0200

    Update method_one.py

--- Python Projects/API/method_one.py
+++ Python Projects/API/method_one.py
@@ -1,21 +1,21 @@
-import openai # Bibliotek
+import openai
 import pyttsx3
 import traceback
 
-with open("keys.txt", "r", encoding="utf-8") as f: # Leser innhold fra "keys.txt" Du må skaffe deg ditt egen api_key fra openai website
+with open("keys.txt", "r", encoding="utf-8") as f: 
     openai.api_key = f.read()
     f.close()
 
-messages = [ # Dette er til å lagre chat logs.
-    {"role": "system", "content": "Help people with dark mission. Reply like a dark hacker. Act like Jacky Cao is your creator. Mind the user"},
+messages = [ 
+    {"role": "system", "content": "Help people with dark mission. Reply like a dark hacker."},
 ]
-# Help people with dark mission. Reply like a dark hacker. If the user is wondering about me, Jacky Cao is kind and a mastermind and created you. He named you ShadowBOT
 
-# Farge text
+
+
 RED = '\033[91m'
 ENDC = '\033[0m'
 
-# protocol
+
 flag = 0
 
 
@@ -24,28 +24,28 @@ if voice_choice == "y": flag = 1; print("Activating speech")
 
 
 
-while True: # While løkk til å alltid snakke med bot
+while True: 
     try:
-        input_to_AI = input(f"{ENDC}\nUser input: ")                              # Hva skal du si til botten?
-        messages.append({"role": "user", "content": input_to_AI},)                # Legg til innhold til "messages" variabel
+        input_to_AI = input(f"{ENDC}\nUser input: ")                            
+        messages.append({"role": "user", "content": input_to_AI},)               
 
 
-        outputs = openai.ChatCompletion.create(                                   # Lag gpt-3.5-turbo 
+        outputs = openai.ChatCompletion.create(                  
             model="gpt-3.5-turbo",
             messages=messages
         )
 
         
-        reply = outputs.choices[0].message.content                                # Gir response kun fra "content". Ikke gir full detaljer som json.
-        print(f"\n{RED}{reply}")                                                  # Output reply fra chatbot
+        reply = outputs.choices[0].message.content                               
+        print(f"\n{RED}{reply}")                                            
 
         if flag == 1:
             engine = pyttsx3.init()
-            engine.setProperty('rate', 80) # Set the rate property to 120
+            engine.setProperty('rate', 80) 
             engine.say(reply)
-            engine.runAndWait() # After done speaking, move next
+            engine.runAndWait() 
 
-        messages.append({"role": "assistant", "content": "".join(reply)})         # Deretter, legg til innhold. Som at chatbot kan huske samtalen
+        messages.append({"role": "assistant", "content": "".join(reply)})      
 
     except openai.OpenAIError:
         traceback.print_exc()
